ee1df4b0cb9b38245bd171e048a8f8eb6763f995
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-12 11:46:39 +0600

    app is done

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser);
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from Express</h1>');
+});
+
+module.exports = app;
