17562fb1f445adf1097c357f0a85b37853339d17
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-13 16:12:16 +0600

    Copyright added in Footer

---
+++ views/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import Copyright from './Copyright';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='container'>
+        <Copyright />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
