19ad8940e663f8a37694d0b8a6599791fa3b12a5
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-14 10:38:44 +0600

    dotenv config and multiple alias

--- views/vite.config.js
+++ views/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,6 +13,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      public: `${path.resolve(__dirname, './public/')}`,
+      pages: path.resolve(__dirname, './src/pages'),
+      assets: `${path.resolve(__dirname, './src/assets')}`,
+      layouts: `${path.resolve(__dirname, './src/layouts')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks')}`,
     },
+  },
+  define: {
+    'process.env': process.env,
   },
 });
