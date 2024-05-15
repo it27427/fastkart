fdce3a68bd697f43294c9a9ea6acc7fb81ec6248
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-15 13:03:39 +0600

    import path fixed

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // AUTH-PAGES
-import LoginPage from '@/pages/auth/LoginPage';
-import RegisterPage from '@/pages/auth/RegisterPage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
 
 // IMPORT-PAGES
-import HomePage from '@/pages/HomePage';
-import ShopPage from '@/pages/ShopPage';
-import ErrorPage from '@/pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import ShopPage from './pages/ShopPage';
+import ErrorPage from './pages/ErrorPage';
 
 const App = () => {
   return (
@@ -15,8 +15,12 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+
+        {/* AUTH-PAGES */}
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+
+        {/* NOT-FOUND-PAGE */}
         <Route path='/*' element={<ErrorPage />} />
       </Routes>
     </Router>
