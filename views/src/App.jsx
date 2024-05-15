dd1510a064384c99500a18c7313cd4a5aec8c2ff
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-15 12:57:29 +0600

    pages added in App

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -1,10 +1,25 @@
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// AUTH-PAGES
+import LoginPage from '@/pages/auth/LoginPage';
+import RegisterPage from '@/pages/auth/RegisterPage';
+
+// IMPORT-PAGES
+import HomePage from '@/pages/HomePage';
+import ShopPage from '@/pages/ShopPage';
+import ErrorPage from '@/pages/ErrorPage';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/*' element={<ErrorPage />} />
+      </Routes>
+    </Router>
   );
 };
 
