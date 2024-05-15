ed5be4bb1785f83c4bcf9b10a8ef96bcd05b3308
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-15 12:27:42 +0600

    vite-config updated

--- views/vite.config.js
+++ views/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import dotenv from 'dotenv';
-import daisyui from 'daisyui';
 
 dotenv.config();
 
