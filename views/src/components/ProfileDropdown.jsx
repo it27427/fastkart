164ce7ecf10af8a45b20389a7fde85ea5a40a5f4
it27427 <istiak.tushar.qtec@gmail.com>
2024-05-17 12:18:04 +0600

    profile-dropdown

--- views/src/components/ProfileDropdown.jsx
+++ views/src/components/ProfileDropdown.jsx
@@ -1,24 +1,26 @@
+import { useState } from 'react';
 import avatar from '../assets/images/avatar/avatar.jpg';
 
 const ProfileDropdown = () => {
+  const [showAvatar, setShowAvatar] = useState(false);
+
   return (
     <>
       <div className='dropdown dropdown-end'>
         <div tabIndex={0} role='button'>
-          <div className='flex items-center'>
-            <div className='btn btn-ghost btn-circle'>
+          {showAvatar ? (
+            <figure className='btn btn-ghost btn-circle'>
               <img
                 src={avatar}
                 alt='User-Avatar'
                 className='w-10 rounded-full avatar'
               />
-            </div>
-            <div className='hidden lg:flex flex-col'>
-              <p className='text-xs font-bold'>Istiak Hossain Tushar</p>
-              <p className='text-xs'>istiak.tushar.it@gmail.com</p>
-            </div>
-          </div>
+            </figure>
+          ) : (
+            <h2>Test</h2>
+          )}
         </div>
+
         <ul
           tabIndex={0}
           className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 dark:bg-slate-900'
